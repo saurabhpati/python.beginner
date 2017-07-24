71aade61938ce96c4110453cb4bc9e7a4a0c1cba
Saurabh <saurabhpati.pati@gmail.com>
2017-07-24 22:07:02 +0530

    math and round functions.

--- Math/numberfunctions.py
+++ Math/numberfunctions.py
@@ -1,8 +1,23 @@
 # Use of random library.
-
+# This library is a very good way to test your code with some random number array. 
 import random;
 
-
 numberList = [143, 222, 332, 501]; 
-print(random.choice(numberList));   # Picks a random number from the list.
-print(random.randrange(1, 100, 2)); # print a random odd number between 1-100.
+
+# choice picks a random number from the list.
+print(random.choice(numberList));
+
+# randrange here prints a random odd number between 1-100.
+# syntax of randrange is randrange(start, stop, step). 
+# start is 0 by default. stop is required. step is 1 by default.
+print(random.randrange(1, 100, 2));
+
+# This will print a random number between 1 to 100.
+print(random.randrange(100));
+
+# Shuffle will shuffle the given list. Gives different output on each run.
+pristineSuite = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'];
+random.shuffle(pristineSuite);
+print('shuffled suite', pristineSuite);
+
+# uniform(x, y) generates a float value >= x and < y
---
+++ Math/roundOff.py
@@ -0,0 +1,7 @@
+# round function rounds the value upto the given input decimals.
+# example round(100.12) will give 100, round(100.29,1) will give 100.3
+# round(100.264, 2) will give 100.26
+
+print(round(100.8)); # 101
+print(round(100.264, 3)); # 100.264
+print(round(100.29, 1)); # 100.3
